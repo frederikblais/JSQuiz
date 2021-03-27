2aa539fad3127de93ff6bc9669d9ca3c71c1a587
unknown <frederik.blais22@gmail.com>
2021-03-27 12:06:47 -0400

    Json + trending

---
+++ app/scripts/trending.js
@@ -0,0 +1,16 @@
+/*
+
+
+var highestValue = 0; //keep track of highest value
+
+//loop through array of objects
+for (var i=0, len = ary.length; i<len; i++) {
+  var value = Number(ary[i]["ppg"]);
+  if (value > highestValue) {
+      highestValue = value;
+  }
+}
+
+
+
+*/
